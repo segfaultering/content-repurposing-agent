6f64e4ae259a6475b372ea683434eeb8723d204d
segfaultering <261435586+segfaultering@users.noreply.github.com>
2026-09-19 17:37:09 +0500

    Add structured output

--- src/content_repurposing_agent/deps.py
+++ src/content_repurposing_agent/deps.py
@@ -1,10 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import BaseModel
 
 from content_repurposing_agent.settings import settings
 
 
+class StructOutput(BaseModel):
+    content: list[str]
+
+
 def get_llm() -> ChatGoogleGenerativeAI:
     return ChatGoogleGenerativeAI(
-        model=settings.model, 
-        api_key=settings.api_key.get_secret_value()
+        model=settings.model, api_key=settings.api_key.get_secret_value()
     )
--- src/content_repurposing_agent/main.py
+++ src/content_repurposing_agent/main.py
@@ -1,17 +1,22 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-from content_repurposing_agent.deps import get_llm
+from content_repurposing_agent.deps import get_llm, StructOutput
 from content_repurposing_agent.settings import ENCODING, SYS_PROMPT
 
 
 def main() -> None:
     llm = get_llm()
+    struct_llm = llm.with_structured_output(StructOutput)
     sys_prompt_text = SYS_PROMPT.read_text(encoding=ENCODING)
     prompt = ChatPromptTemplate.from_messages(
         [("system", sys_prompt_text), ("user", "{user_input}")]
     )
-    chain = prompt | llm
+    chain = prompt | struct_llm
     user_input = input("Paste Content Here:\n")
     response = chain.invoke({"user_input": user_input})
 
-    print(response)
+    print(response.content)
+
+
+if __name__ == "__main__":
+    main()
